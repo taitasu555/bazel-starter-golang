4cbb7c88cc1fc87e4d02fa78b498635fe54c7582
taitasu555 <taitasu08035@gmail.com>
2025-02-06 16:59:10 +0900

    add cobara

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 
 	"github.com/google/uuid"
+	"github.com/spf13/cobra"
 	stringutil "github.com/taitasu555/bazel-starter-golang/pkg/string"
 )
 
@@ -11,5 +15,26 @@ func main() {
 	uuidStr := uuid.NewString()
 	reverceStr := stringutil.Reverse(uuidStr)
 	fmt.Printf("uuid: %s\nreverce: %s\n", uuidStr, reverceStr)
+
+	command := &cobra.Command{
+		Use:   "server",
+		Short: "Start the server",
+		Run: func(cmd *cobra.Command, args []string) {
+			//local server start
+			h1 := func(w http.ResponseWriter, _ *http.Request) {
+				io.WriteString(w, "Hello from a HandleFunc #1!\n")
+			}
+			h2 := func(w http.ResponseWriter, _ *http.Request) {
+				io.WriteString(w, "Hello from a HandleFunc #2!\n")
+			}
+
+			http.HandleFunc("/", h1)
+			http.HandleFunc("/endpoint", h2)
+
+			log.Fatal(http.ListenAndServe(":8080", nil))
+		},
+	}
+
+	command.Execute()
 
 }
